82b811e172ffe4202b5762e2b20d99617648d5e3
arnaud-jobexit <arnaud@jobexit.fr>
2022-10-13 16:43:52 +0200

    code to track pageviews

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env:{
+    GOOGLE_ANALYTICS_ID:'G-0MK1K6T7YS'
+  }
 }
 
 module.exports = nextConfig
---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+export default function AboutPage() {
+    return (
+        <div>
+            <Head>
+                <title>About Page</title>
+            </Head>
+            <h1>About Page</h1>
+        </div>
+    )
+}
